ded910206dfbe66d6ea473714c633b572dcd22b4
Гусейн <huseinsadigov@gmail.com>
2026-07-17 15:35:10 +0300

    feat: add controller

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springframework:spring-context:6.1.5")
     implementation("org.springframework:spring-tx:6.1.5")
+    implementation("org.springframework:spring-web:6.1.5")
 }
 
 tasks.test {
