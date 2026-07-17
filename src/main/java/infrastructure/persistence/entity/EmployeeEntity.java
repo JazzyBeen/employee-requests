14d6ed5f3cdfe6443d35434a9c0e26c9d2344c27
Гусейн <huseinsadigov@gmail.com>
2026-07-17 15:19:55 +0300

    feat: add persistence entity

---
+++ src/main/java/infrastructure/persistence/entity/EmployeeEntity.java
@@ -0,0 +1,25 @@
+package infrastructure.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false)
+    private String position;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id", nullable = false)
+    private DepartmentEntity department;
+}
